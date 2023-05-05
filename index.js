daa254aa1aa7e9ee36678b5d96c830b46d3dddb4
Nils Matic <matic1909@gmail.com>
2023-05-05 12:56:44 +0200

    refactored book card creation into own fucntion

--- index.js
+++ index.js
@@ -18,33 +18,39 @@ function addBookToLibrary() {
   myLibrary.push(newBook);
 }
 
+function makeBookCard(book) {
+  const readText = book.read ? 'not read yet' : 'read';
+
+  const card = document.createElement('div');
+  const bookTitle = document.createElement('h2');
+  const bookAuthor = document.createElement('p');
+  const bookPages = document.createElement('p');
+  const bookRead = document.createElement('p');
+
+  bookTitle.classList.add('book-title');
+  bookTitle.textContent = book.title;
+
+  bookAuthor.classList.add('book-author');
+  bookAuthor.textContent = book.author;
+
+  bookPages.classList.add('book-pages');
+  bookPages.textContent = `${book.pages} pages`;
+
+  bookRead.classList.add('book-read');
+  bookRead.textContent = readText;
+
+  card.appendChild(bookTitle);
+  card.appendChild(bookAuthor);
+  card.appendChild(bookPages);
+  card.appendChild(bookRead);
+
+  return card;
+}
+
 function renderBooks() {
   const libraryContainer = document.querySelector('.library-container');
   myLibrary.forEach((book) => {
-    const readText = book.read ? 'not read yet' : 'read';
-
-    const card = document.createElement('div');
-    const bookTitle = document.createElement('h2');
-    const bookAuthor = document.createElement('p');
-    const bookPages = document.createElement('p');
-    const bookRead = document.createElement('p');
-
-    bookTitle.classList.add('book-title');
-    bookTitle.textContent = book.title;
-
-    bookAuthor.classList.add('book-author');
-    bookAuthor.textContent = book.author;
-
-    bookPages.classList.add('book-pages');
-    bookPages.textContent = `${book.pages} pages`;
-
-    bookRead.classList.add('book-read');
-    bookRead.textContent = readText;
-
-    card.appendChild(bookTitle);
-    card.appendChild(bookAuthor);
-    card.appendChild(bookPages);
-    card.appendChild(bookRead);
+    const card = makeBookCard(book);
     libraryContainer.appendChild(card);
   });
 }
