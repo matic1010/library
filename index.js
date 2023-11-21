9a6c9a560761793483c119db57defc5573c4170e
Nils Matic <matic1909@gmail.com>
2023-11-21 14:59:55 +0100

    added button to remove book from library

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ const myLibrary = [
   new Book("Test 3", "Timo Brudi", 90, false)
 ];
 
+const newBookButton = document.getElementById("new-book-button");
+
+newBookButton.addEventListener("click", () => {
+  const addBookModal = document.getElementById("new-book-modal");
+  addBookModal.showModal();
+});
+
+const addBookButton = document.getElementById("add-book-button");
+
+addBookButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const titleInput = document.getElementById("title");
+  const authorInput = document.getElementById("author");
+  const pagesInput = document.getElementById("pages");
+  const readInput = document.getElementById("read");
+
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  const read = readInput.checked;
+
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.checked = false;
+
+  const addBookModal = document.getElementById("new-book-modal");
+  addBookModal.close();
+
+  const bookToSave = {title, author, pages, read};
+
+  addBookToLibrary(bookToSave);
+  renderBooks();
+})
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -17,25 +52,8 @@ Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${!this.read ? 'not yet' : ""} read`;
 }
 
-function addBookToLibrary() {
-  let title, author, pages, read;
-  while(!title) {
-    title = prompt("What is the title of the book?");
-  }
-  while(!author) {
-    author = prompt("Who wrote it?");
-  }
-  while(!pages) {
-    const input = Number(prompt("How many pages does it have?"))
-    if(Number.isInteger(input)) {
-      pages = input;
-    } else {
-      alert('Please input a number');
-    }
-  }
-  read = confirm("Have you read it?");
-
-  myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary({title, author, pages, read}) {
+    myLibrary.push(new Book(title, author, pages, read));
 }
 
 function makeBookCard(book) {
@@ -58,14 +76,27 @@ function makeBookCard(book) {
   readStatus.textContent = `${book.read ? 'read' : 'not yet read'}`
   card.appendChild(readStatus);
 
+  const removeButton = document.createElement("button");
+  removeButton.addEventListener("click", (e) => {
+    removeBookFromLibrary(Number(e.target.parentElement.dataset.index));
+    renderBooks();
+  });
+  removeButton.innerHTML = "Remove Book"
+  card.appendChild(removeButton);
+
   return card;
+}
+
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1)
 }
 
 function renderBooks() {
   const booksDiv = document.getElementById("books");
   booksDiv.innerHTML = "";
-  myLibrary.forEach(book => {
+  myLibrary.forEach((book, index) => {
     const bookCard = makeBookCard(book);
+    bookCard.dataset.index = index;
     booksDiv.appendChild(bookCard)
   })
 }
