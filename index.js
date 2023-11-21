5671a52960ba453c7da4e92b968720a5bc7a08ac
Nils Matic <matic1909@gmail.com>
2023-11-21 11:50:46 +0100

    fixed addBookToLibrary function

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.info = () => `${title} by ${author}, ${pages} pages, ${!read ? 'not yet' : ""} read`;
+Book.prototype.info = function() {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${!this.read ? 'not yet' : ""} read`;
+}
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary() {
+  let title, author, pages, read;
+  while(!title) {
+    title = prompt("What is the title of the book?");
+  }
+  while(!author) {
+    author = prompt("Who wrote it?");
+  }
+  while(!pages) {
+    const input = Number(prompt("How many pages does it have?"))
+    if(Number.isInteger(input)) {
+      pages = input;
+    } else {
+      alert('Please input a number');
+    }
+  }
+  read = confirm("Have you read it?");
+
+  myLibrary.push(new Book(title, author, pages, read));
 }
