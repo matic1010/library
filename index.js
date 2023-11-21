71868ee32eb62dbd28b0d95a4f2a623c921e33ed
Nils Matic <matic1909@gmail.com>
2023-11-21 11:38:56 +0100

    added function for adding books

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = () => `${title} by ${author}, ${pages} pages, ${!read ? 'not yet' : ""} read`;
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
