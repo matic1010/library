1cb0c8cfd01635deb700bd478afe3f34d9263898
Nils Matic <matic1909@gmail.com>
2023-11-22 10:32:39 +0100

    changed onject constructors to classes

--- index.js
+++ index.js
@@ -41,19 +41,21 @@ addBookButton.addEventListener("click", (e) => {
   renderBooks();
 })
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  
+  info() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${!this.read ? 'not yet' : ""} read`;
+  }
 
-Book.prototype.info = function() {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${!this.read ? 'not yet' : ""} read`;
-}
-
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
 
 function addBookToLibrary({title, author, pages, read}) {
